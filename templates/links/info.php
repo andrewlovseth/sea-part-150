e95e3094d5703c63f37ac59d1e571e3393a3596e
Andrew Lovseth <andrew.lovseth@me.com>
2024-05-24 13:31:57 -0700

    Styling updates

--- templates/links/info.php
+++ templates/links/info.php
@@ -3,15 +3,16 @@
     $info = get_field('info');
     $copy = $info['copy'];
 
-?>
+    if ($copy):
+ 
+ ?>
 
-<section class="info | grid">
+    <section class="info | grid">
 
-    <?php if ($copy): ?>
         <div class="copy p1">
             <?php echo $copy; ?>
         </div>
-    <?php endif; ?>
 
-</section>
+    </section>
 
+<?php endif; ?>
