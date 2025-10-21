dc6f9b473f25f9a7f9940ab667edcd913a5089c5
Andrew Lovseth <andy.lovseth@gmail.com>
2025-10-21 09:16:48 -0700

    Add URL resources

--- templates/resources/documents-list.php
+++ templates/resources/documents-list.php
@@ -11,11 +11,20 @@
         <?php 
             $title = get_the_title();
             $description = get_field('description');
-            $file = get_field('file');
-            $file_type = $file['subtype'];
-            $file_size = $file['filesize'];
-            $date_obj = DateTime::createFromFormat('Y-m-d H:i:s', $file['modified']);
-            $date = $date_obj->format('m/d/Y');
+            $type = get_field('type');
+            
+            // Get the appropriate URL and file info based on type
+            if ($type === 'url') {
+                $link_url = get_field('url');
+                $file_type = null;
+                $file_size = null;
+            } else {
+                // Default to 'document' type
+                $file = get_field('file');
+                $link_url = $file['url'];
+                $file_type = isset($file['subtype']) ? $file['subtype'] : null;
+                $file_size = isset($file['filesize']) ? $file['filesize'] : null;
+            }
 
             $terms = wp_get_object_terms($post->ID, 'group');
             $document_class = 'document';
@@ -26,8 +35,14 @@
         ?>
 
         <div class="<?php echo $document_class; ?>">
-            <a href="<?php echo $file['url']; ?>" target="window">
-                <?php get_template_part('template-parts/svg/pdf'); ?>
+            <a href="<?php echo $link_url; ?>" target="window">
+                <?php 
+                    if ($type === 'url') {
+                        get_template_part('template-parts/svg/link');
+                    } else {
+                        get_template_part('template-parts/svg/pdf');
+                    }
+                ?>
 
                 <div class="info">
                     <div class="cat-header">
@@ -46,11 +61,12 @@
                         </div>
                     <?php endif; ?>
 
-                    <div class="meta copy p3 sans-serif">
-                        <?php if($file_size): ?>             
+
+                    <?php if($file_size): ?>    
+                        <div class="meta copy p3 sans-serif">
                             <span class="file-size"><strong>File Size:</strong> <em><?php echo formatBytes($file_size, 2); ?></em></span>
-                        <?php endif; ?>
-                    </div>
+                        </div>
+                    <?php endif; ?>
 
 
                 </div>
